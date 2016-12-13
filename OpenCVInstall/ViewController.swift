e7a6f563975371146445ec8599faccf2589d9949
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-12-13 11:32:10 +0800

    Initial Commit

---
+++ OpenCVInstall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenCVInstall
+//
+//  Created by Sgmedical on 2016/12/13.
+//  Copyright © 2016年 Sgmedical. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
